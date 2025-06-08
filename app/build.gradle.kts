d0ad5daeae07e13160287dec8664202cdc568c6c
Maary <24504742+Steve-Mr@users.noreply.github.com>
2025-06-08 15:47:18 +0800

    bump version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 2
-        versionName = "2025.04.04-alpha-01"
+        versionName = "2025.06.08-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
